924c36ab5465bf8959c6a413d4d38e0da3002d5a
Rishabh <satya@janaspandana.in>
2023-01-09 18:06:24 +0530

    Add colors

--- lib/resources/colors.dart
+++ lib/resources/colors.dart
@@ -2,15 +2,23 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 
 class MyColors {
-  static const primaryColor = Color(0xFF68C689);
+  static const primaryColor = Color(0xFF0052CC);
   static const secondaryColor = Color(0xFF24272C);
-  static const accentColor1 = Color(0xFF388E3C);
-  static const accentColor2 = Color(0xFF66BB6A);
-  static const accentColor3 = Color(0xFF81C784);
+  static const accentColor1 = Color(0xFF0d47a1);
+  static const accentColor2 = Color(0xFF0c4091);
+  static const accentColor3 = Color(0xFF0a3981);
   static const background = Color(0xFF121212);
   static const divider = Color(0xFF1111111A);
   static const card = Color(0xFFE6F8FF);
   static const border = Color(0xFF111129);
-  static const red = Color(0xFFF2353C);
-  static const containerHeaderColor = Color(0xFFE4F5EA);
+  static const red = Color(0xFFFF3333);
+  static const green = Color(0xFF397D54);
+  static const paleBlue = Color(0xFFECF7FF);
+  static const lightBlue = Color(0xFFCFF9FF);
+  static const cerulean = Color(0xFF1268AE);
+  static const navyBlue = Color(0xFF04457A);
+  static const lightGreen = Color(0xFF1B9B20);
+  static const paleGreen = Color(0xFFECFFEE);
+  static const yellow = Color(0xFFFFCE5B);
+  static const glacier = Color(0xFFD5DFE6);
 }
