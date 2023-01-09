f867c22c7c415a7eabf92b10410c505ea5e31105
Rishabh <satya@janaspandana.in>
2023-01-09 18:14:27 +0530

    Add images

--- lib/resources/images.dart
+++ lib/resources/images.dart
@@ -1,9 +1,12 @@
 class Images {
   ///icons
   static const calender = 'assets/icons/calendar.png';
+  static const editIcon = 'assets/icons/edit.png';
 
   ///images
   static const logo = 'assets/images/logo.png';
+  static const onBoardingImage = 'assets/images/onboarding.png';
   static const animatedLogo = 'assets/images/animated_logo.gif';
-  static const leaveSuccess = 'assets/leave_success.png';
+  static const success = 'assets/images/success.json';
+
 }
