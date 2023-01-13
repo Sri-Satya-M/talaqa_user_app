1530dbfd97efdee905a07d9f780da4a2b27ee9d4
Rishabh <satya@janaspandana.in>
2023-01-13 11:14:38 +0530

    Design patient_card

--- lib/ui/screens/main/sessions/widgets/patient_card.dart
+++ lib/ui/screens/main/sessions/widgets/patient_card.dart
@@ -1,5 +1,8 @@
 import 'package:alsan_app/resources/colors.dart';
+import 'package:alsan_app/ui/screens/main/menu/reports/widgets/time_slot.dart';
+import 'package:alsan_app/ui/widgets/avatar.dart';
 import 'package:alsan_app/ui/widgets/custom_card.dart';
+import 'package:alsan_app/ui/widgets/details_tile.dart';
 import 'package:flutter/material.dart';
 
 class PatientCard extends StatefulWidget {
@@ -20,21 +23,87 @@ class _PatientCardState extends State<PatientCard> {
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Text("ALSOO34"),
+                Text(
+                  "ALSOO34",
+                  style: textTheme.headline4,
+                ),
                 ElevatedButton(
-                  onPressed: () {},
+                  onPressed: null,
                   style: ElevatedButton.styleFrom(
+                    disabledBackgroundColor: MyColors.yellow,
                     backgroundColor: MyColors.yellow,
                     foregroundColor: MyColors.secondaryColor,
                     fixedSize: const Size(140, 40),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5),
+                    ),
                   ),
                   child: Text(
-                    'Pending',
+                    'PENDING',
+                    style: textTheme.bodyText1,
                   ),
                 ),
               ],
             ),
-
+            const SizedBox(height: 20),
+            Row(
+              children: [
+                const Avatar(
+                  url:
+                      'https://miro.medium.com/fit/c/88/88/1*0HhsaB_S9yiF-hi9AESZTg.jpeg',
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(20),
+                  ),
+                ),
+                const SizedBox(width: 8),
+                DetailsTile(
+                  title: const Text("Dr. Ruchika Mittal"),
+                  value: Text(
+                    "20 years exp",
+                    style: textTheme.caption,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 12),
+            Row(
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("Patient", style: textTheme.caption),
+                    const SizedBox(height: 4),
+                    Text(
+                      "ALex Oliver",
+                      style: textTheme.headline3
+                          ?.copyWith(fontWeight: FontWeight.w400),
+                    ),
+                  ],
+                ),
+                const SizedBox(width: 102),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("Speciality", style: textTheme.caption),
+                    const SizedBox(height: 4),
+                    Text(
+                      "Speech Therapy",
+                      style: textTheme.headline3
+                          ?.copyWith(fontWeight: FontWeight.w400),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            const SizedBox(height: 14),
+            Text("Mode of consultation", style: textTheme.caption),
+            const SizedBox(height: 5),
+            Text(
+              "At Home",
+              style: textTheme.headline3?.copyWith(fontWeight: FontWeight.w400),
+            ),
+            const SizedBox(height: 14),
+            TimeSlot()
           ],
         ),
       ),
