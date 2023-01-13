347ac22589c1272945c30b9511b8ed55223785f3
Rishabh <satya@janaspandana.in>
2023-01-13 10:55:46 +0530

    Add images

--- lib/resources/images.dart
+++ lib/resources/images.dart
@@ -3,10 +3,16 @@ class Images {
   static const calender = 'assets/icons/calendar.png';
   static const editIcon = 'assets/icons/edit.png';
   static const profileIcon = 'assets/icons/profile.png';
+  static const sessionsIcon = 'assets/icons/sessions.png';
+  static const reportIcon = 'assets/icons/report.png';
+  static const supportIcon = 'assets/icons/support.png';
+  static const referIcon = 'assets/icons/refer.png';
+  static const faqIcon = 'assets/icons/faq.png';
   static const phone = 'assets/icons/phone.png';
   static const email = 'assets/icons/email.png';
   static const patient = 'assets/icons/patient.png';
   static const pendingSession = 'assets/icons/pendingSession.png';
+  static const voice = 'assets/icons/voice.png';
 
 
   ///images
@@ -14,5 +20,6 @@ class Images {
   static const onBoardingImage = 'assets/images/onboarding.png';
   static const animatedLogo = 'assets/images/animated_logo.gif';
   static const success = 'assets/images/success.json';
+  static const referImage = 'assets/images/referImage.png';
 
 }
