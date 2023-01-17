7092e277026d7435ba0b77c538aa2c57b9ef0018
Rishabh <satya@janaspandana.in>
2023-01-17 11:51:07 +0530

    Add profile & patient profile model

--- lib/model/profile.dart
+++ lib/model/profile.dart
@@ -1,49 +1,57 @@
 class Profile {
   Profile({
     this.id,
+    this.fullName,
     this.image,
     this.age,
     this.gender,
     this.city,
     this.country,
     this.userId,
+    this.patientId,
     this.createdAt,
     this.updatedAt,
     this.user,
   });
 
   int? id;
+  String? fullName;
   String? image;
   int? age;
   String? gender;
   String? city;
-  dynamic country;
+  String? country;
   int? userId;
+  int? patientId;
   DateTime? createdAt;
   DateTime? updatedAt;
   User? user;
 
   factory Profile.fromJson(Map<String, dynamic> json) => Profile(
     id: json["id"],
+    fullName: json["fullName"],
     image: json["image"],
     age: json["age"],
     gender: json["gender"],
     city: json["city"],
     country: json["country"],
     userId: json["userId"],
-    createdAt: DateTime.parse(json["createdAt"]),
-    updatedAt: DateTime.parse(json["updatedAt"]),
-    user: User.fromJson(json["user"]),
+    patientId: json["patientId"],
+    createdAt: json["createdAt"]==null ? null: DateTime.parse(json["createdAt"]),
+    updatedAt: json["updatedAt"]==null ? null: DateTime.parse(json["updatedAt"]),
+    user: json["user"] == null ? null: User?.fromJson(json["user"]),
   );
 
   Map<String, dynamic> toJson() => {
     "id": id,
+    "fullName": fullName,
     "image": image,
     "age": age,
     "gender": gender,
     "city": city,
     "country": country,
     "userId": userId,
+    "patientId": patientId,
     "createdAt": createdAt?.toIso8601String(),
     "updatedAt": updatedAt?.toIso8601String(),
     "user": user!.toJson(),
