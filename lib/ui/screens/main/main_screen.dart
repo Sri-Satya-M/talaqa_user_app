72951f7235d654a6a3a64769100b5e4d5fa2f0ba
Rishabh <rishabh@janaspandana.in>
2023-01-18 15:53:40 +0530

    Merge main_screen

--- lib/ui/screens/main/main_screen.dart
+++ lib/ui/screens/main/main_screen.dart
@@ -4,8 +4,9 @@ import 'package:alsan_app/resources/colors.dart';
 import 'package:alsan_app/resources/theme.dart';
 import 'package:alsan_app/ui/screens/main/browse/browse_screen.dart';
 import 'package:alsan_app/ui/screens/main/home/home_screen.dart';
-
 import 'package:alsan_app/ui/screens/main/menu/menu_screen.dart';
+import 'package:alsan_app/ui/screens/main/resources/resources_screen.dart';
+import 'package:alsan_app/ui/screens/main/sessions/session_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -15,7 +16,7 @@ class MainScreen extends StatefulWidget {
   static Future open(BuildContext context) {
     return Navigator.of(context).push(
       MaterialPageRoute(
-        builder: (context) => MainScreen(),
+        builder: (context) => const MainScreen(),
       ),
     );
   }
@@ -25,16 +26,17 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
-  String getTitile(int index) {
+  List<Widget> tabBars = [
+    const Center(child: Text("Actions")),
+    const Center(child: Text("Videos")),
+  ];
+
+  int tabLength(int index) {
     switch (index) {
-      case 0:
-        return 'Home';
-      case 1:
-        return 'Browse';
-      case 2:
-        return 'Sessions';
+      case 3:
+        return 2;
     }
-    return 'NA';
+    return 0;
   }
 
   @override
@@ -42,76 +44,99 @@ class _MainScreenState extends State<MainScreen> {
     var mainBloc = Provider.of<MainBloc>(context, listen: false);
     var userBloc = Provider.of<UserBloc>(context, listen: false);
     var textTheme = Theme.of(context).textTheme;
-    return Scaffold(
-      appBar: AppBar(
-        titleSpacing: 16,
-        title: Text(userBloc.profile?.user?.fullName ?? ''),
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: Icon(
-              Icons.notifications_active_outlined,
-              color: AppTheme.primaryColor,
+    return DefaultTabController(
+      length: tabLength(mainBloc.index),
+      child: Scaffold(
+        appBar: AppBar(
+          titleSpacing: 16,
+          title: Text(userBloc.profile?.user?.fullName ?? ''),
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.notifications_active_outlined,
+                color: AppTheme.primaryColor,
+              ),
+            ),
+          ],
+          bottom: PreferredSize(
+            preferredSize:
+                Size.fromHeight(tabLength(mainBloc.index) > 0 ? 40 : 0),
+            child: Builder(
+              builder: (context) {
+                switch (mainBloc.index) {
+                  case 3:
+                    return const TabBar(
+                      tabs: [
+                        Tab(text: 'Articles'),
+                        Tab(text: 'Videos'),
+                      ],
+                    );
+
+                  default:
+                    return const SizedBox();
+                }
+              },
             ),
           ),
-        ],
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        landscapeLayout: BottomNavigationBarLandscapeLayout.spread,
-        type: BottomNavigationBarType.fixed,
-        currentIndex: mainBloc.index,
-        onTap: (value) {
-          setState(() {
-            mainBloc.index = value;
-          });
-        },
-        selectedItemColor: MyColors.cerulean,
-        unselectedItemColor: Colors.grey,
-        showSelectedLabels: true,
-        selectedLabelStyle: textTheme.caption?.copyWith(
-          color: MyColors.cerulean,
         ),
-        unselectedLabelStyle: textTheme.caption?.copyWith(color: Colors.grey),
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home_outlined),
-            label: "Home",
+        bottomNavigationBar: BottomNavigationBar(
+          landscapeLayout: BottomNavigationBarLandscapeLayout.spread,
+          type: BottomNavigationBarType.fixed,
+          currentIndex: mainBloc.index,
+          onTap: (value) {
+            setState(() {
+              mainBloc.index = value;
+            });
+          },
+          selectedItemColor: MyColors.cerulean,
+          unselectedItemColor: Colors.grey,
+          showSelectedLabels: true,
+          selectedLabelStyle: textTheme.caption?.copyWith(
+            color: MyColors.cerulean,
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.search_outlined),
-            label: "Browse",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.watch_later_outlined),
-            label: "Sessions",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.language),
-            label: "Resources",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.menu_rounded),
-            label: "Menu",
-          ),
-        ],
-      ),
-      body: Builder(
-        builder: (context) {
-          switch (mainBloc.index) {
-            case 0:
-              return HomeScreen();
-            case 1:
-              return BrowseScreen();
-            case 2:
-              return Container();
-            case 3:
-              return Container();
-            case 4:
-              return MenuScreen();
-            default:
-              return Container();
-          }
-        },
+          unselectedLabelStyle: textTheme.caption?.copyWith(color: Colors.grey),
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home_outlined),
+              label: "Home",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.search_outlined),
+              label: "Browse",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.watch_later_outlined),
+              label: "Sessions",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.language),
+              label: "Resources",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.menu_rounded),
+              label: "Menu",
+            ),
+          ],
+        ),
+        body: Builder(
+          builder: (context) {
+            switch (mainBloc.index) {
+              case 0:
+                return HomeScreen();
+              case 1:
+                return BrowseScreen();
+              case 2:
+                return SessionScreen();
+              case 3:
+                return ResourcesScreen();
+              case 4:
+                return const MenuScreen();
+              default:
+                return Container();
+            }
+          },
+        ),
       ),
     );
   }
