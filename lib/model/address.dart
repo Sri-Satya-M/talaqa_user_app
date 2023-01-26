612e27badd7ccfef09009a10a589617bb8e65d68
yashwanth <yashwanth@janaspandana.in>
2023-01-27 00:48:46 +0530

    Add address model

---
+++ lib/model/address.dart
@@ -0,0 +1,63 @@
+class Address {
+  Address({
+    this.id,
+    this.latitude,
+    this.longitude,
+    this.addressLine1,
+    this.addressLine2,
+    this.landmark,
+    this.pincode,
+    this.city,
+    this.country,
+    this.mobileNumber,
+    this.patientId,
+    this.createdAt,
+    this.updatedAt,
+  });
+
+  int? id;
+  String? latitude;
+  String? longitude;
+  String? addressLine1;
+  String? addressLine2;
+  String? landmark;
+  String? pincode;
+  String? city;
+  String? country;
+  String? mobileNumber;
+  int? patientId;
+  DateTime? createdAt;
+  DateTime? updatedAt;
+
+  factory Address.fromMap(Map<String, dynamic> json) => Address(
+        id: json["id"],
+        latitude: json["latitude"],
+        longitude: json["longitude"],
+        addressLine1: json["addressLine1"],
+        addressLine2: json["addressLine2"],
+        landmark: json["landmark"],
+        pincode: json["pincode"],
+        city: json["city"],
+        country: json["country"],
+        mobileNumber: json["mobileNumber"],
+        patientId: json["patientId"],
+        createdAt: DateTime.parse(json["createdAt"]),
+        updatedAt: DateTime.parse(json["updatedAt"]),
+      );
+
+  Map<String, dynamic> toMap() => {
+        "id": id,
+        "latitude": latitude,
+        "longitude": longitude,
+        "addressLine1": addressLine1,
+        "addressLine2": addressLine2,
+        "landmark": landmark,
+        "pincode": pincode,
+        "city": city,
+        "country": country,
+        "mobileNumber": mobileNumber,
+        "patientId": patientId,
+        "createdAt": createdAt?.toIso8601String(),
+        "updatedAt": updatedAt?.toIso8601String(),
+      };
+}
