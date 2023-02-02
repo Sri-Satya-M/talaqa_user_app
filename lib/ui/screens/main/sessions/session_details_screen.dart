8f1dc0ec10e3c3107854e8663701e9b856776819
yashwanth <yashwanth@janaspandana.in>
2023-02-02 22:25:33 +0530

    UI changes

--- lib/ui/screens/main/sessions/session_details_screen.dart
+++ lib/ui/screens/main/sessions/session_details_screen.dart
@@ -15,6 +15,7 @@ import '../../../widgets/empty_widget.dart';
 import '../../../widgets/error_widget.dart';
 import '../../../widgets/loading_widget.dart';
 import '../../../widgets/reverse_details_tile.dart';
+import '../home/booking/payment_screen.dart';
 import '../home/booking/widgets/details_box.dart';
 import '../home/booking/widgets/timeslot_details_widget.dart';
 
@@ -38,6 +39,8 @@ class SessionDetailsScreen extends StatefulWidget {
 }
 
 class _SessionDetailsScreenState extends State<SessionDetailsScreen> {
+  bool enablePay = false;
+
   @override
   Widget build(BuildContext context) {
     var textTheme = Theme.of(context).textTheme;
@@ -47,111 +50,148 @@ class _SessionDetailsScreenState extends State<SessionDetailsScreen> {
         title: const Text('Session Details'),
       ),
       body: FutureBuilder<Session>(
-          future: sessionBloc.getSessionById(widget.session.id.toString()),
-          builder: (context, snapshot) {
-            if (snapshot.hasError) {
-              return CustomErrorWidget(error: snapshot.error);
-            }
-            if (!snapshot.hasData) return const LoadingWidget();
-            var data = snapshot.data;
-            if (data == null) return const EmptyWidget();
+        future: sessionBloc.getSessionById(widget.session.id.toString()),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return CustomErrorWidget(error: snapshot.error);
+          }
+          if (!snapshot.hasData) return const LoadingWidget();
+          var session = snapshot.data;
+          if (session == null) return const EmptyWidget();
 
-            return ListView(
-              padding: const EdgeInsets.all(20),
-              shrinkWrap: true,
-              scrollDirection: Axis.vertical,
-              physics: const ScrollPhysics(),
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                    border: Border.all(color: MyColors.divider),
-                    borderRadius: BorderRadius.circular(5),
-                  ),
-                  padding: const EdgeInsets.all(8),
-                  child: Row(
+          return ListView(
+            padding: const EdgeInsets.all(20),
+            shrinkWrap: true,
+            scrollDirection: Axis.vertical,
+            physics: const ScrollPhysics(),
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: MyColors.divider),
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                padding: const EdgeInsets.all(8),
+                child: Row(
+                  children: [
+                    const Text('Session ID'),
+                    const Spacer(),
+                    Container(
+                      padding: const EdgeInsets.all(8),
+                      decoration: BoxDecoration(
+                        color: const Color(0xFF5BFF9F),
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      child: Text('${widget.session.sessionId}'),
+                    ),
+                  ],
+                ),
+              ),
+              PatientDetailsWidget(patient: widget.session.patientProfile!),
+              ClinicianDetailsWidget(clinician: widget.session.clinician!),
+              DetailsBox(
+                title: 'Session Details',
+                child: Padding(
+                  padding: const EdgeInsets.all(12),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
                     children: [
-                      const Text('Session ID'),
-                      const Spacer(),
-                      Container(
-                        padding: const EdgeInsets.all(8),
-                        decoration: BoxDecoration(
-                          color: const Color(0xFF5BFF9F),
-                          borderRadius: BorderRadius.circular(5),
+                      DynamicGridView(
+                        count: 2,
+                        spacing: 8,
+                        children: [
+                          ReverseDetailsTile(
+                            title: const Text('Specialty'),
+                            value: Text(
+                              '${widget.session.clinician?.designation}',
+                              style: textTheme.bodyText1,
+                            ),
+                          ),
+                          ReverseDetailsTile(
+                            title: const Text('Mode of consultation'),
+                            value: Text(
+                              '${widget.session.consultationMode}',
+                              style: textTheme.bodyText1,
+                            ),
+                          ),
+                          ReverseDetailsTile(
+                            title: const Text('Duration'),
+                            value: Text(
+                              'Lorem',
+                              style: textTheme.bodyText1,
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 8),
+                      TimeslotDetailsWidget(
+                        dateTime: widget.session.date!,
+                        timeslots: TimeSlot()
+                            .showTimeslots(widget.session.clinicianTimeSlots!),
+                      ),
+                      const SizedBox(height: 8),
+                      ReverseDetailsTile(
+                        title: const Text('Description'),
+                        value: Text(
+                          widget.session.description!,
+                          style: textTheme.bodyText1,
                         ),
-                        child: Text('${widget.session.sessionId}'),
                       ),
                     ],
                   ),
                 ),
-                PatientDetailsWidget(patient: widget.session.patientProfile!),
-                ClinicianDetailsWidget(clinician: widget.session.clinician!),
-                DetailsBox(
-                  title: 'Session Details',
-                  child: Padding(
-                    padding: const EdgeInsets.all(12),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: [
-                        DynamicGridView(
-                          count: 2,
-                          spacing: 8,
-                          children: [
-                            ReverseDetailsTile(
-                              title: const Text('Specialty'),
-                              value: Text(
-                                '${widget.session.clinician?.designation}',
-                                style: textTheme.bodyText1,
-                              ),
-                            ),
-                            ReverseDetailsTile(
-                              title: const Text('Mode of consultation'),
-                              value: Text(
-                                '${widget.session.consultationMode}',
-                                style: textTheme.bodyText1,
-                              ),
-                            ),
-                            ReverseDetailsTile(
-                              title: const Text('Duration'),
-                              value: Text(
-                                'Lorem',
-                                style: textTheme.bodyText1,
-                              ),
-                            ),
-                          ],
-                        ),
-                        const SizedBox(height: 8),
-                        TimeslotDetailsWidget(
-                          dateTime: widget.session.date!,
-                          timeslots: TimeSlot().showTimeslots(
-                              widget.session.clinicianTimeSlots!),
-                        ),
-                        const SizedBox(height: 8),
-                        ReverseDetailsTile(
-                          title: const Text('Description'),
-                          value: Text(
-                            widget.session.description!,
-                            style: textTheme.bodyText1,
+              ),
+              (widget.session.consultationMode == "HOME")
+                  ? AddressCard(address: widget.session.patientAddress!)
+                  : const SizedBox(),
+              DetailsBox(
+                title: 'Consultation Bill Details',
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(12),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "Consultation fee (${session.totalAmount} Slots) Fee",
+                            style: textTheme.subtitle2
+                                ?.copyWith(color: Colors.black.withOpacity(1)),
                           ),
-                        ),
-                      ],
+                          Text(
+                            "${session.totalAmount} Dirham",
+                            style: textTheme.subtitle2
+                                ?.copyWith(color: Colors.black.withOpacity(1)),
+                          )
+                        ],
+                      ),
                     ),
-                  ),
+                    const Divider(),
+                    Padding(
+                      padding: const EdgeInsets.all(15),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Text("Total Amount"),
+                          Text("${session.totalAmount} Dirham")
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
-                (widget.session.consultationMode == "HOME")
-                    ? AddressCard(address: widget.session.patientAddress!)
-                    : const SizedBox(),
-                TimelineWidget(statuses: widget.session.sessionStatuses!),
-                const SizedBox(height: 64),
+              ),
+              TimelineWidget(statuses: widget.session.sessionStatuses!),
+              const SizedBox(height: 16),
+              if (session.status == "APPROVED") ...[
+                ProgressButton(
+                  onPressed: () {
+                    PaymentScreen.open(context, session: session);
+                  },
+                  child: const Text('Pay Now'),
+                ),
               ],
-            );
-          }),
-      extendBody: true,
-      bottomNavigationBar: Padding(
-        padding: const EdgeInsets.all(20),
-        child: ProgressButton(
-          onPressed: () {},
-          child: const Text('Pay Now'),
-        ),
+            ],
+          );
+        },
       ),
     );
   }
