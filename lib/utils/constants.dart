7f4d9bfbee63d772477024d8874d9734d611f9fa
Yashwanth <yashwanth@janaspandana.in>
2023-02-16 20:48:53 +0530

    Add constants file

---
+++ lib/utils/constants.dart
@@ -0,0 +1,6 @@
+class Constants {
+  static const String appId = "968ae33c48f744f1bcc1b7f120348c74";
+  static const String project = "alsan";
+  static const String primaryCertificatte = "307ce5f5910b44e8a378c02421c22b35";
+  static const String secondaryCertificate = "c6a19d773a224c05ab1cad880f9e0772";
+}
