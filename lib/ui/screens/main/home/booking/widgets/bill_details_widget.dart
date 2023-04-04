7c62d84100d6bcee67bfb0c0716d8fa2700f1dbd
Yashwanth <yashwanth@janaspandana.in>
2023-04-04 17:14:02 +0530

    Design bill_details_widget

---
+++ lib/ui/screens/main/home/booking/widgets/bill_details_widget.dart
@@ -0,0 +1,58 @@
+import 'package:dotted_line/dotted_line.dart';
+import 'package:flutter/material.dart';
+
+import '../../../../../../resources/colors.dart';
+
+class BillDetailsWidget extends StatelessWidget {
+  final int noOfTimeslots;
+  final double totalAmount;
+  final String consultationMode;
+
+  const BillDetailsWidget({
+    super.key,
+    required this.noOfTimeslots,
+    required this.totalAmount,
+    required this.consultationMode,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    var textTheme = Theme.of(context).textTheme;
+    return Container(
+      padding: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        color: MyColors.cementShade2,
+        borderRadius: BorderRadius.circular(5),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Text('Consultation Bill Details', style: textTheme.bodyText1),
+          const SizedBox(height: 8),
+          const Divider(),
+          const SizedBox(height: 8),
+          Row(
+            children: [
+              Expanded(
+                child: Text(
+                  'Consultation fee ($noOfTimeslots $consultationMode Slot${noOfTimeslots > 1 ? 's' : ''})',
+                ),
+              ),
+              Text('$totalAmount Dihram'),
+            ],
+          ),
+          const SizedBox(height: 12),
+          const DottedLine(dashGapLength: 4, dashColor: MyColors.divider),
+          const SizedBox(height: 16),
+          Row(
+            children: [
+              const Text('Total Amount'),
+              const Spacer(),
+              Text('$totalAmount Dirham'),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
