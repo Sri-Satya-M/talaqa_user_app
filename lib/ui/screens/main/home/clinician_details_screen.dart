10f4d66ae9a7c44441535ee8d4f8362744972857
Sri Satya <satya@janaspandana.in>
2023-06-09 11:46:42 +0530

    Refactor code & api clinician_details_screen.dart

--- lib/ui/screens/main/home/clinician_details_screen.dart
+++ lib/ui/screens/main/home/clinician_details_screen.dart
@@ -1,17 +1,17 @@
-import 'package:alsan_app/bloc/language_bloc.dart';
-import 'package:alsan_app/bloc/sesssion_bloc.dart';
-import 'package:alsan_app/bloc/user_bloc.dart';
-import 'package:alsan_app/model/clinicians.dart';
-import 'package:alsan_app/ui/widgets/details_tile.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import '../../../../bloc/language_bloc.dart';
+import '../../../../bloc/sesssion_bloc.dart';
+import '../../../../bloc/user_bloc.dart';
+import '../../../../model/clinicians.dart';
 import '../../../../model/review.dart';
 import '../../../../model/service.dart';
 import '../../../../resources/colors.dart';
 import '../../../../resources/images.dart';
 import '../../../../resources/strings.dart';
 import '../../../widgets/avatar.dart';
+import '../../../widgets/details_tile.dart';
 import '../../../widgets/empty_widget.dart';
 import '../../../widgets/error_widget.dart';
 import '../../../widgets/loading_widget.dart';
@@ -19,7 +19,7 @@ import 'booking/widgets/service_card.dart';
 import 'clinician_review_screen.dart';
 import 'widgets/review_card.dart';
 
-class ClinicianDetailsScreen extends StatelessWidget {
+class ClinicianDetailsScreen extends StatefulWidget {
   final Clinician clinician;
 
   const ClinicianDetailsScreen({super.key, required this.clinician});
@@ -33,140 +33,165 @@ class ClinicianDetailsScreen extends StatelessWidget {
   }
 
   @override
+  State<ClinicianDetailsScreen> createState() => _ClinicianDetailsScreenState();
+}
+
+class _ClinicianDetailsScreenState extends State<ClinicianDetailsScreen> {
+  Future<List>? futures;
+
+  @override
+  void initState() {
+    super.initState();
+    var sessionBloc = Provider.of<SessionBloc>(context, listen: false);
+    var userBloc = Provider.of<UserBloc>(context, listen: false);
+    futures = Future.wait([
+      sessionBloc.getServices(query: {'clinicianId': widget.clinician.id}),
+      userBloc.getReview(query: {
+        'offset': 0,
+        'limit': 10,
+        'clinicianId': widget.clinician.id.toString()
+      }),
+    ]);
+  }
+
+  @override
   Widget build(BuildContext context) {
     var textTheme = Theme.of(context).textTheme;
-    var userBloc = Provider.of<UserBloc>(context, listen: false);
     var langBloc = Provider.of<LangBloc>(context, listen: false);
-    var sessionBloc = Provider.of<SessionBloc>(context, listen: false);
     return Scaffold(
       appBar: AppBar(title: Text(langBloc.getString(Strings.clinicianDetails))),
-      body: ListView(
-        shrinkWrap: true,
-        padding: const EdgeInsets.all(20),
-        physics: const ScrollPhysics(),
-        children: [
-          Row(
+      body: FutureBuilder<List>(
+        future: futures,
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return CustomErrorWidget(error: snapshot.error);
+          }
+          if (!snapshot.hasData) return const LoadingWidget();
+          var list = snapshot.data ?? [];
+          return ListView(
+            padding: const EdgeInsets.all(20),
             children: [
-              Avatar(
-                url: clinician.imageUrl,
-                name: clinician.user?.fullName,
-                borderRadius: BorderRadius.circular(10),
-                size: 72,
+              Row(
+                children: [
+                  Avatar(
+                    url: widget.clinician.imageUrl,
+                    name: widget.clinician.user?.fullName,
+                    borderRadius: BorderRadius.circular(10),
+                    size: 72,
+                  ),
+                  const SizedBox(width: 16),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          widget.clinician.user?.fullName ?? 'NA',
+                          style: textTheme.bodyText2,
+                        ),
+                        Text(
+                          widget.clinician.designation ?? 'NA',
+                          style: textTheme.caption?.copyWith(
+                            color: MyColors.cerulean,
+                          ),
+                        ),
+                        const SizedBox(height: 8),
+                        Text(
+                          '${widget.clinician.experience} '
+                          '${langBloc.getString(Strings.yearsExp)}',
+                          style: textTheme.subtitle2,
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
-              const SizedBox(width: 16),
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  mainAxisAlignment: MainAxisAlignment.center,
+              const SizedBox(height: 16),
+              DetailsTile(
+                title: const Text('Languages Known'),
+                value: Row(
                   children: [
-                    Text(
-                      clinician.user?.fullName ?? 'NA',
-                      style: textTheme.bodyText2,
-                    ),
-                    Text(
-                      clinician.designation ?? 'NA',
-                      style: textTheme.caption?.copyWith(
-                        color: MyColors.cerulean,
+                    Image.asset(Images.voice, width: 12),
+                    const SizedBox(width: 8),
+                    Expanded(
+                      child: Text(
+                        widget.clinician.languagesKnown ?? 'NA',
+                        overflow: TextOverflow.ellipsis,
                       ),
-                    ),
-                    const SizedBox(height: 8),
-                    Text(
-                      '${clinician.experience} ${langBloc.getString(Strings.yearsExp)}',
-                      style: textTheme.subtitle2,
                     ),
                   ],
                 ),
               ),
-            ],
-          ),
-          const SizedBox(height: 16),
-          DetailsTile(
-            title: const Text('Languages Known'),
-            value: Row(
-              children: [
-                Image.asset(Images.voice, width: 12),
-                const SizedBox(width: 8),
-                Expanded(
-                  child: Text(
-                    clinician.languagesKnown ?? 'NA',
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                ),
-              ],
-            ),
-          ),
-          const SizedBox(height: 16),
-          DetailsTile(
-            title: Text(langBloc.getString(Strings.bio)),
-            value: Text(clinician.bio ?? 'NA'),
-          ),
-          const SizedBox(height: 16),
-          Text(langBloc.getString(Strings.services)),
-          FutureBuilder<Services>(
-            future: sessionBloc.getServices(query: {'clinicianId': clinician.id}),
-            builder: (context, snapshot) {
-              if (snapshot.hasError) {
-                return CustomErrorWidget(error: snapshot.error);
-              }
-
-              if (!snapshot.hasData) return const LoadingWidget();
-
-              var services = snapshot.data?.services ?? [];
-
-              if (services.isEmpty) return const EmptyWidget();
-
-              return ListView.builder(
-                itemCount: services.length,
-                shrinkWrap: true,
-                physics: const ScrollPhysics(),
-                itemBuilder: (context, index) => ServiceCard(
-                  service: services[index],
-                ),
-              );
-            },
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                langBloc.getString(Strings.reviews),
-                style: textTheme.bodyText1,
+              const SizedBox(height: 16),
+              DetailsTile(
+                title: Text(langBloc.getString(Strings.bio)),
+                value: Text(widget.clinician.bio ?? 'NA'),
               ),
-              TextButton(
-                onPressed: () => ClinicianReviewsScreen.open(context,
-                    id: clinician.id.toString()),
-                child: Text(
-                  langBloc.getString(Strings.seeAll),
-                  style: textTheme.headline2?.copyWith(
-                    color: MyColors.primaryColor,
-                  ),
-                ),
-              ),
+              const SizedBox(height: 16),
+              Builder(builder: (context) {
+                List<Service> services = list[0]?.services ?? [];
+                if (services.isEmpty) {
+                  return const EmptyWidget();
+                } else {
+                  return Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(langBloc.getString(Strings.services)),
+                      ListView.builder(
+                        itemCount: services.length,
+                        shrinkWrap: true,
+                        physics: const NeverScrollableScrollPhysics(),
+                        itemBuilder: (context, index) {
+                          return ServiceCard(service: services[index]);
+                        },
+                      ),
+                    ],
+                  );
+                }
+              }),
+              const SizedBox(height: 30),
+              Builder(builder: (context) {
+                List<Review> reviews = list[1] ?? [];
+                if (reviews.isEmpty) {
+                  return const EmptyWidget();
+                } else {
+                  return Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            langBloc.getString(Strings.reviews),
+                            style: textTheme.bodyText1,
+                          ),
+                          TextButton(
+                            onPressed: () => ClinicianReviewsScreen.open(
+                                context,
+                                id: widget.clinician.id.toString()),
+                            child: Text(
+                              langBloc.getString(Strings.seeAll),
+                              style: textTheme.headline2?.copyWith(
+                                color: MyColors.primaryColor,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      ListView.builder(
+                        itemCount: reviews.length,
+                        shrinkWrap: true,
+                        physics: const NeverScrollableScrollPhysics(),
+                        itemBuilder: (context, index) {
+                          return ReviewCard(review: reviews[index]);
+                        },
+                      ),
+                    ],
+                  );
+                }
+              }),
             ],
-          ),
-          FutureBuilder<List<Review>>(
-            future: userBloc.getReview(query: {
-              'offset':0,
-              'limit':10,
-              'clinicianId': clinician.id.toString()
-            }),
-            builder: (context, snapshot) {
-              if (snapshot.hasError) {
-                return CustomErrorWidget(error: snapshot.error);
-              }
-              if (!snapshot.hasData) return const LoadingWidget();
-              var reviews = snapshot.data ?? [];
-              if (reviews.isEmpty) return const EmptyWidget();
-              return ListView.builder(
-                itemCount: reviews.length,
-                shrinkWrap: true,
-                itemBuilder: (context, index) {
-                  return ReviewCard(review: reviews[index]);
-                },
-              );
-            },
-          ),
-        ],
+          );
+        },
       ),
     );
   }
