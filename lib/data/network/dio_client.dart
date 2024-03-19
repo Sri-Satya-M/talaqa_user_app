f26f3a420466644a03f45b8d5e614b15f059a7f3
Sri Satya <satya@janaspandana.in>
2024-03-19 13:16:22 +0530

    version: 0.1.13+13

--- lib/data/network/dio_client.dart
+++ lib/data/network/dio_client.dart
@@ -10,7 +10,7 @@ class DioClient implements ApiClient {
   late Dio dio;
   String accessToken = "Bearer fiakb9318uishfsksn5oibbynn86gb3w";
   String baseUrl =
-      kReleaseMode ? 'https://api.talaqa.online' : 'http://10.0.0.121:7001';
+      kReleaseMode ? 'https://api.talaqa.online' : 'http://10.0.0.105:7001';
 
   String mapsKey = 'AIzaSyBTQ-RrVQbGz2ymNmdSkenm8vlkVR8Q_ec';
 
